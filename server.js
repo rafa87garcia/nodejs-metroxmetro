c177dafa0ff8150bffef7be787370728d4536e52
Rafael Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-02-13 13:01:28 +0100

    Control de errores

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
+// Control de errores
+app.use((err, _req, res, _next) => {
+  return res
+      .status(err.status || 500)
+      .json(err.message || 'Error inesperado en el servidor');
+
+});
 
 app.listen(config.PORT, () => {
   console.log("Node server listening on port 3000");
