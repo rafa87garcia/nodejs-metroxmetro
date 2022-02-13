6a0f080ad939271f3d361706ed619829198ed4c8
Rafael Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-02-13 13:02:00 +0100

    Todas las urls 404

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
+
+app.use('*', (req, res, next) => {
+  const error = new Error('Ruta no encotrada');
+  error.status = 404;
+  return next(error);
+});
+
 // Control de errores
 app.use((err, _req, res, _next) => {
   return res
