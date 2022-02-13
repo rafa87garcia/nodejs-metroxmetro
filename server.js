8ff6ad63f7b4cad6375f49a74f55febcaef2e912
Rafael Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-02-13 13:03:23 +0100

    json body

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
+// Enabled body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => res.status(200).send("Server running"));
 
 
