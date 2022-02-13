91bae374e61580a5d214488fb80027d5f236f048
Rafael Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-02-13 13:52:38 +0100

    Router land

--- Routers/land.router.js
+++ Routers/land.router.js
@@ -5,90 +5,77 @@ const Land = require('../models/Land');
 
 const router = express.Router();
 
-router.get('/', (req_, res, next) => {
+router.get('/', (req, res, next) => {
+    let filter = {};
 
-    return Land.find()
+    if (req.query.name) {
+        filter = { ...filter, name: { $regex: req.query.name } };
+    }
+    console.log(filter);
+    return Land.find(filter)
         .then(land => {
+            return res.status(200).json(land);
+        })
+});
+
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    return Land.findById(id)
+        .then(land => {
+            if (!land) {
+                const error = new Error('Land not found');
+                error.status = 404;
+                return next(error);
+            }
             return res.status(200).json(land);
         })
         .catch(err => {
             const error = new Error(err);
-            error.status = 404;
-            next(error);
+            error.status = 500;
+            return next(error);
+        });
+});
+
+router.post('/', (req, res, next) => {
+    const newLand = new Movie(req.body);
+
+    return newLand.save()
+        .then(() => {
+            return res.status(201).json("New land");
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.status = 500;
+            return next(error);
         })
 });
 
-// router.get('/', (req, res) => {
-//     console.log("user->user", req.user);
-//     let filter = {};
 
-//     if (req.query.nombre) {
-//         filter = { ...filter, nombre: { $regex: req.query.nombre } };
-//     }
-//     console.log(filter);
-//     return Movie.find(filter)
-//         .then(movie => {
-//             return res.status(200).json(movie);
-//         })
-// });
+router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    
+    Land.findOneAndUpdate(id, { $set: req.body }, { new: true })
+        .then((land) => {
+            return res.status(200).json(land);
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.status = 500;
+            return next(error);
+        })
+})
 
-// router.get('/:id', (req, res, next) => {
-//     const id = req.params.id;
-//     return Movie.findById(id)
-//         .then(movie => {
-//             if (!movie) {
-//                 const error = new Error('Movie not found');
-//                 error.status = 404;
-//                 return next(error);
-//             }
-//             return res.status(200).json(movie);
-//         })
-//         .catch(err => {
-//             const error = new Error(err);
-//             error.status = 500;
-//             return next(error);
-//         });
-// });
-
-// router.post('/', (req, res, next) => {
-//     const newMovie = new Movie(req.body);
-
-//     return newMovie.save()
-//         .then(() => {
-//             return res.status(201).json("Nueva pelicula");
-//         })
-//         .catch(err => {
-//             const error = new Error(err);
-//             error.status = 500;
-//             return next(error);
-//         })
-// });
-
-
-// router.put('/:id', (req, res, next) => {
-//     const id = req.params.id;
-//     Movie.findOneAndUpdate(id, { $set: req.body }, { new: true })
-//         .then((movie) => {
-//             return res.status(200).json(movie);
-//         })
-//         .catch(err => {
-//             const error = new Error(err);
-//             error.status = 500;
-//             return next(error);
-//         })
-// })
-
-// router.delete('/:id', (req, res, next) => {
-//     const id = req.params.id;
-//     Movie.findByIdAndDelete(id)
-//         .then(() => {
-//             return res.status(200).json(`Movie deleted ${id}`);
-//         })
-//         .catch(err => {
-//             const error = new Error(err);
-//             error.status = 500;
-//             return next(error);
-//         })
-// });
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    Land.findByIdAndDelete(id)
+        .then(() => {
+            return res.status(200).json(`Land deleted ${id}`);
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.status = 500;
+            return next(error);
+        })
+});
 
 module.exports = router;
