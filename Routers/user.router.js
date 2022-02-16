3a4d062c3a9f5b3260bad8e0a3c9b50c7acc48af
Rafael Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-02-16 20:40:46 +0100

    Añadir login

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-require('../authentication/passport');
+const { signIn } = require('../authentication/jsonwebtoken');
 
 const router = express.Router();
 
@@ -21,6 +21,34 @@ router.post('/register', (req, res, next) => {
   }
 
   passport.authenticate('register', callback)(req);
+});
+
+router.post('/login', (req, res, next) => {
+  const callback = (error, user) => {
+    if (error) {
+      console.log("Error al entrar al login", error);
+      return next(error);
+    }
+
+    req.logIn(user, (errorLogin => {
+      if (errorLogin) {
+        return next(errorLogin);
+      }
+      const token = new signIn(user, req.app.get('jwt-secret'));
+      res.status(201).json({ userId: user._id, token });
+    }));
+  }
+
+  passport.authenticate('login', callback)(req);
+});
+
+router.post('/logout', (req, res, next) => {
+  if (!req.user) {
+    return res.sendStatus(301);
+  }
+  req.logOut();
+
+  return res.status(200).json("User session close");
 });
 
 module.exports = router;
