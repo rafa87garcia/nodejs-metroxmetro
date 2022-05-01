33ef1480e6d308478bd048a30364de38ed425286
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-01 18:35:04 +0200

    Cambios mínimos

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
-const config = require('./config');
 
 const express = require('express');
-const db = require('./db');
-
 const passport = require('passport');
-require('./authentication/passport');
-
-//const auth = require('./middlewares/auth.middleware');
-
-const app = express();
-
-// app.set('jwt-secret', config.JWT_SECRET);
-
+const config = require('./config');
 const landRouter = require('./Routers/land.router');
 const userRouter = require('./Routers/user.router');
+const db = require('./db');
+require('./authentication/passport')
+
+const server = express();
+
 
 // Enabled body
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
-// Añadimos el middleware de passport a express
-app.use(passport.initialize());
+server.use(passport.initialize());
 
-app.get('/', (_req, res) => res.status(200).send("Server running"));
+server.get('/', (_req, res) => {
+  res.status(200).send("Server running")
+});
 
-app.use('/land', landRouter);
-app.use('/users', userRouter);
+server.use('/lands', landRouter);
+server.use('/users', userRouter);
 
-app.use('*', (req, res, next) => {
+server.use('*', (_req, _res, next) => {
   const error = new Error('Ruta no encotrada');
   error.status = 404;
   return next(error);
 });
 
 // Control de errores
-app.use((err, _req, res, _next) => {
+server.use((err, _req, res, _next) => {
   return res
     .status(err.status || 500)
     .json(err.message || 'Error inesperado en el servidor');
@@ -43,7 +39,7 @@ app.use((err, _req, res, _next) => {
 
 db.connectDB.then(() => {
   console.log("Conection to database.");
-  app.listen(config.PORT, () => {
+  server.listen(config.PORT, () => {
     console.log("Node server listening on port 3000");
   });
 
