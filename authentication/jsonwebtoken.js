c1f3d2eeca59160e316bea2ea658d8c19e85d9fc
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-01 16:17:35 +0200

    Añadir datos payload

--- authentication/jsonwebtoken.js
+++ authentication/jsonwebtoken.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const syncIn = (user, secret) => {
+const signIn = (user, secret) => {
   const token = jwt.sign(
     {
       uid: user._id,
+      name: user.name,
       email: user.email,
       rol: user.rol,
     },
     secret,
-    { expiresIn: '5m' }
+    { expiresIn: '1h' }
   );
-  return token;
-}
 
-module.exports = { syncIn } 
+  return token;
+};
+
+module.exports = { signIn };
