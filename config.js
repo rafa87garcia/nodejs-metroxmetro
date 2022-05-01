ddd7360b43868a281f6ece74ee4d36b0a54d8c4d
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-01 18:34:35 +0200

    Nueva configuración

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+// const dotenv = require('dotenv');
+// dotenv.config();
 require('dotenv').config();
 
 const config = {
-  PORT: process.env.PORT,
-  URL_DB: process.env.URL_DB,
-  JWT_SECRET: process.env.JWT_SECRET
-}
+  PORT: process.env.PORT || 3000,
+  DB_URL: process.env.DB_URL || 'mongodb+srv://admin:admin@upgrade-nov-2021.kuhpa.mongodb.net/metroxmetro?retryWrites=true&w=majority',
+  JWT_SECRET: process.env.JWT_SECRET || 'secreto-para-desarrollo',
+};
 
 module.exports = config;
