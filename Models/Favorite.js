f158fc643a72f7755f387280bd05cbc1ac618b53
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-02 19:38:14 +0200

    Cambiar propiedad a un solo valor

--- Models/Favorite.js
+++ Models/Favorite.js
@@ -3,7 +3,7 @@ const { Schema, model, Types } = require('mongoose');
 
 const favoriteSchema = Schema({
     user: { type: Types.ObjectId, ref: 'User', require: true },
-    property: [{ type: Types.ObjectId, ref: 'Property', require: true }],
+    property: { type: Types.ObjectId, ref: 'Property', require: true },
 }, {
     timestamps: true,
 }
