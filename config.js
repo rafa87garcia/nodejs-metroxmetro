13fda52bcce79fe829b2fc3cefd54d292f5145a2
Rafa Garcia Rea <garcia.rea@bootcamp-upgrade.com>
2022-05-02 11:13:54 +0200

    Cambiar dotenv

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
-require('dotenv').config();
-
+const dotenv = require('dotenv');
+dotenv.config();
 const config = {
   PORT: process.env.PORT || 3000,
   DB_URL: process.env.DB_URL,
